5787ed50d9ebac5f14a6f543b8e30298e4292ee7
= <=>
2026-05-14 09:28:59 +0200

    Karakter osztaly kontruktora elkezdve

---
+++ Karakter.js
@@ -0,0 +1,22 @@
+export default class Karakter {
+    #pozicio = {};
+    #hatizsak;
+
+    constructor(x, y) {
+        this.#pozicio = {x: x, y : y};
+        this.#hatizsak = [];
+        
+    }
+
+    mozog() {
+
+    }
+
+    felvesz() {
+
+    }
+
+    megjelenit() {
+
+    }
+}
