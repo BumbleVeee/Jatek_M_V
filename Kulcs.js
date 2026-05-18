75e9c28d6c830dc68b26710b748e0e2c578f9825
BumbleVeee <gubek.veronika.eva@gmail.com>
2026-05-19 00:11:10 +0200

    kulcs osztaly ellenorzesre var

--- Kulcs.js
+++ Kulcs.js
@@ -1,11 +1,20 @@
-export default class Kulcs{
-    constructor(x, y, kep = ""){
+export default class Kulcs {
+
+    constructor(x, y, kep = "", szelesseg = 32, magassag = 32) {
+
+        this.tipus = "kulcs";
+
         this.pozicio = {
-            x: x,
-            y: y
-        }
+            x, y
+        };
+
+        this.meret = {
+            szelesseg,
+            magassag
+        };
 
         this.kep = kep;
-    }
 
+        this.felveheto = true;
+    }
 }
