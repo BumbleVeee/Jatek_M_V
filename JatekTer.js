4dc30eb9eccce03e30273a26f48a72bcdee830d6
= <=>
2026-05-20 15:18:33 +0200

    JatekTer félig kész, +adat.js a képekhez

--- JatekTer.js
+++ JatekTer.js
@@ -1,17 +1,70 @@
+import Karakter from "./Karakter.js";
+import Kulcs from "./Kulcs.js";
+import Ajto from "./Ajto.js";
+import { KEPLISTA } from "./adat.js";
+
 export default class JatekTer {
-    #ajto;
-    #kulcs;
-    #magassag;
-    #szelesseg;
-    constructor() {
-        
+  #karakter;
+  #ajto;
+  #kulcs;
+  constructor() {
+    this.szuloElem = document.querySelector("jatekter");
+    this.#karakter = new Karakter(50, 50, KEPLISTA[0].kep);
+    this.#kulcs = new Kulcs(300, 200, KEPLISTA[1].kep);
+    this.#ajto = new Ajto(600, 150, KEPLISTA[2].kep);
+
+    this.input = {
+      left: false,
+      right: false,
+      up: false,
+      down: false,
+    };
+
+    this.esemenyek();
+
+    this.megjelenit();
+  }
+
+  megjelenit() {
+    this.#karakter.megjelenit(this.szuloElem);
+
+    this.#kulcs.megjelenit(this.szuloElem);
+
+    this.#ajto.megjelenit(this.szuloElem);
+  }
+
+  update() {
+    this.mozgas();
+
+    this.kulcsFelvetel();
+
+    this.ajtoNyitas();
+
+    this.#karakter.frissit();
+  }
+
+  mozgas() {
+    if (this.input.left) {
+      this.#karakter.pozicio.x -= 2;
     }
 
+    if (this.input.right) {
+      this.#karakter.pozicio.x += 2;
+    }
 
+    if (this.input.up) {
+      this.#karakter.pozicio.y -= 2;
+    }
+
+    if (this.input.down) {
+      this.#karakter.pozicio.y += 2;
+    }
+  }
 }
 
-
 /*
+nem tudjuk hogy szükséges ezek közül valamelyik :(
+
 for (let y = 0; y < this.magassag; y++) {
 
     for (let x = 0; x < this.szelesseg; x++) {
---
+++ adat.js
@@ -0,0 +1,11 @@
+export const KEPLISTA = [
+    {
+        kep : "kepek/bean.png" /* karakter képe */
+    },
+    {
+        kep : "kepek/kulcs.jpeg" /* kulcs képe */
+    },
+    {
+        kep : "kepek/ajto.png" /* ajto képe */
+    }
+]
