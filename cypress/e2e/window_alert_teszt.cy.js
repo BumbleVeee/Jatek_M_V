4269564dd77156ac0246bf133d20fd0f5c86c961
BumbleVeee <gubek.veronika.eva@gmail.com>
2026-05-28 00:00:51 +0200

    cypress kepek megjelenese

--- cypress/e2e/window_alert_teszt.cy.js
+++ cypress/e2e/window_alert_teszt.cy.js
@@ -2,8 +2,34 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://bumbleveee.github.io/Jatek_M_V/')
   })
-  it('kulcs alert', () => {
+  /*it('kulcs alert', () => {
     cy.visit('https://bumbleveee.github.io/Jatek_M_V/')
     
-    })
+    })*/
 })
+
+describe("Képek megjelenése", () => {
+
+    beforeEach(() => {
+        cy.visit("https://bumbleveee.github.io/Jatek_M_V/");
+    });
+
+    it("Karakter kép megjelenik", () => {
+
+        cy.get(".karakter img")
+            .should("exist");
+    });
+
+    it("Kulcs kép megjelenik", () => {
+
+        cy.get(".kulcs img")
+            .should("exist");
+    });
+
+    it("Ajtó kép megjelenik", () => {
+
+        cy.get(".ajto img")
+            .should("exist");
+    });
+});
+
